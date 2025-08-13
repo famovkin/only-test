0a4bfe4deafd475f671c08e76a1e91c4db1ede9a
Tolya Fam <a.fam@onpoint.ru>
2025-08-13 04:32:44 +0300

    add packages (classNames, sass, swiper, stylelint)

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+const config = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-idiomatic-order',
+  ],
+  rules: {
+    'selector-class-pattern': null,
+    'media-feature-range-notation': null,
+  },
+};
+
+module.exports = config;
